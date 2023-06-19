137d68fd585deba6828a04347f60f8203c3baa4d
alastaira <alastair.aitchison@gmail.com>
2023-06-19 14:18:06 +0100

    Update SquawkersMcGraw.ino

--- SquawkersMcGraw/SquawkersMcGraw.ino
+++ SquawkersMcGraw/SquawkersMcGraw.ino
@@ -1,48 +1,61 @@
-
+/**
+ * "Squawkers MGraw" Animatronic Controller
+ */
 
 // INCLUDES
 #include "Button2.h"
 
+// Movement patterns
+// Neutral, (FlapWingsHeadDown), (lookRightHeadUp), (HeadLeft), NeutralFlapWing
+
 // CONSTANTS
-byte headMotorPins[] = {4, 5};
+byte headMotorPins[] = {4, 5}; // Blue/Purple
 byte bodyMotorPins[] = {6, 7};
 byte numInputs = 5;
 byte inputPins[] = {A1, A2, A3, A4, A5};
 byte numSensors = 5;
-byte sensorPins[] = {8, 9, 10, 11, 12};
+const byte tonguePin = 8; // Blue wire to head
+const byte eyesClosedPin = 10; // Orange wire
+const byte eyesOpenPin = 11; // Purple wire
+const byte legPin = 12;
+const byte sensorPins[] = {tonguePin, 9, eyesClosedPin, eyesOpenPin, legPin};
 
 Button2 buttons[5];
 Button2 sensors[5];
 
-
-
 void setup() {
   Serial.begin(115200);
+  Serial.println(__FILE__ __DATE__);
   // put your setup code here, to run once:
   for(int i=0; i<2; i++){
     pinMode(headMotorPins[i], OUTPUT);  
     pinMode(bodyMotorPins[i], OUTPUT);
   }
+  /*
   for(int i=0; i< numInputs; i++){
     pinMode(inputPins[i], INPUT_PULLUP);
     buttons[i].begin(inputPins[i]);
     buttons[i].setPressedHandler(pressed);
     buttons[i].setReleasedHandler(released);
   }
+  */
   for(int i=0; i< numSensors; i++){
     pinMode(sensorPins[i], INPUT_PULLUP);
     sensors[i].begin(sensorPins[i]);
-    sensors[i].setChangedHandler(changed);
+    sensors[i].setDebounceTime(1);
+    //sensors[i].setChangedHandler(changed);
   }
 }
 
 void changed(Button2& btn) {
-  Serial.print("Sensor changed");
+  Serial.print("Sensor changed ");
+  Serial.print(btn.getPin());
+  /*
   for(int i=0; i<numSensors; i++){
     Serial.print(digitalRead(sensorPins[i]));
     Serial.print(i==numSensors-1 ? "\r\n" : ",");
   }
-
+*/
   if(btn == sensors[0]){
     Serial.println(btn.isPressed() ? "Tongue pressed" : "Tongue released");
   }
@@ -54,16 +67,12 @@ void changed(Button2& btn) {
 }
 
 void openEyes(){
-  while(digitalRead(sensorPins[3]) == HIGH) {
-    digitalWrite(headMotorPins[0], HIGH);
-    digitalWrite(headMotorPins[1], LOW);
-  }
+  digitalWrite(headMotorPins[0], HIGH);
+  analogWrite(headMotorPins[1], 128);
 }
 void closeEyes(){
-  while(digitalRead(sensorPins[1]) == HIGH) {
-    digitalWrite(headMotorPins[0], HIGH);
-    digitalWrite(headMotorPins[1], LOW);
-  }
+  analogWrite(headMotorPins[0], HIGH);
+  digitalWrite(headMotorPins[1], 128);
 }
 
 void pressed(Button2& btn) {
@@ -99,7 +108,7 @@ void blink(){
   digitalWrite(headMotorPins[0], HIGH);
   digitalWrite(headMotorPins[1], LOW);
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
+  delay(100);
   digitalWrite(headMotorPins[0], LOW);
   digitalWrite(headMotorPins[1], LOW);
   digitalWrite(LED_BUILTIN, LOW);   
@@ -109,33 +118,63 @@ void talk(){
   digitalWrite(headMotorPins[0], LOW);
   digitalWrite(headMotorPins[1], HIGH);
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(250);
+  delay(50);
   digitalWrite(headMotorPins[0], LOW);
   digitalWrite(headMotorPins[1], LOW);
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void body(){
-  digitalWrite(bodyMotorPins[0], LOW);
-  digitalWrite(bodyMotorPins[1], HIGH);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(bodyMotorPins[0], LOW);
-  digitalWrite(bodyMotorPins[1], LOW);
-  digitalWrite(LED_BUILTIN, LOW);
-}
-
-void body2(){
-  digitalWrite(bodyMotorPins[0], HIGH);
-  digitalWrite(bodyMotorPins[1], LOW);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(bodyMotorPins[0], LOW);
-  digitalWrite(bodyMotorPins[1], LOW);
-  digitalWrite(LED_BUILTIN, LOW);
+// Ends with head low and to the side
+void moveBody(int dir=0){
+  digitalWrite(bodyMotorPins[0], dir);
+  digitalWrite(bodyMotorPins[1], 1-dir);
 }
 
 void loop() {
+  // Read any input
+  int command = -1;
+  if(Serial.available() > 0){
+    // Read any integer sent
+    command = Serial.parseInt();
+  }
+
+  // Test Limit switches
+  // eye pins
+  if(sensors[2].wasPressed()){
+    digitalWrite(headMotorPins[0], LOW);
+    digitalWrite(headMotorPins[1], LOW);
+  }
+  if(sensors[3].wasPressed()){
+    digitalWrite(headMotorPins[0], LOW);
+    digitalWrite(headMotorPins[1], LOW);
+  }
+  // legPin
+  if(sensors[4].wasPressed()){
+    digitalWrite(bodyMotorPins[0], LOW);
+    digitalWrite(bodyMotorPins[1], LOW);
+  }
+
+  switch(command){
+    case 3:
+      blink();
+      break;
+    case 4:
+      talk();
+      break;
+    case 5:
+      openEyes();
+      break;
+    case 6:
+      closeEyes();
+      break;
+    case 7:
+      moveBody(0);
+      break;
+    case 8:
+      moveBody(1);
+      break;
+  }
+
   for(int i=0; i<numInputs; i++){ buttons[i].loop(); }
   for(int i=0; i<numSensors; i++){ sensors[i].loop(); }
 }
